3d9d8539251f1c4d6d51750168ed95766d6dd6bd
gabbyca <gabrielle_cammarata@mynbps.org>
2023-04-21 09:09:27 -0400

    Fixed code

--- face_rec/faceRec.py
+++ face_rec/faceRec.py
@@ -9,43 +9,44 @@ from pytz import timezone
 import face_recognition
 
 
-# camera = 'tcp://10.161.140.140:5000'
-camera = 0
-stream = cv2.VideoCapture(camera)
+
+#camera = 'tcp://192.168.1.252:5000'
+stream = cv2.VideoCapture(0)
 
 #face_recognition
-gabby_image = face_recognition.load_image_file("facesDatabase/Gabby.png")
+gabby_image = face_recognition.load_image_file("FacesDatabase/Gabby.png")
 gabby_face_encoding = face_recognition.face_encodings(gabby_image)[0]
 
-layne_image = face_recognition.load_image_file("facesDatabase/Layne.png")
+layne_image = face_recognition.load_image_file("FacesDatabase/Layne.png")
 layne_face_encoding = face_recognition.face_encodings(layne_image)[0]
 
-sam_image = face_recognition.load_image_file("facesDatabase/Sam.png")
+sam_image = face_recognition.load_image_file("FacesDatabase/Sam.png")
 sam_face_encoding = face_recognition.face_encodings(sam_image)[0]
 
-reid_image = face_recognition.load_image_file("facesDatabase/Reid.png")
+reid_image = face_recognition.load_image_file("FacesDatabase/Reid.png")
 reid_face_encoding = face_recognition.face_encodings(reid_image)[0]
 
-anthony_image = face_recognition.load_image_file("facesDatabase/Anthony.png")
+anthony_image = face_recognition.load_image_file("FacesDatabase/Anthony.png")
 anthony_face_encoding = face_recognition.face_encodings(anthony_image)[0]
 
-bella_image = face_recognition.load_image_file("facesDatabase/Bella.png")
+bella_image = face_recognition.load_image_file("FacesDatabase/Bella.png")
 bella_face_encoding = face_recognition.face_encodings(bella_image)[0]
 
-maya_image = face_recognition.load_image_file("facesDatabase/Maya.png")
+maya_image = face_recognition.load_image_file("FacesDatabase/Maya.png")
 maya_face_encoding = face_recognition.face_encodings(maya_image)[0]
 
-sara_image = face_recognition.load_image_file("facesDatabase/Sara.png")
+sara_image = face_recognition.load_image_file("FacesDatabase/Sara.png")
 sara_face_encoding = face_recognition.face_encodings(sara_image)[0]
 
-tymoffi_image = face_recognition.load_image_file("facesDatabase/Tymoffi.png")
+tymoffi_image = face_recognition.load_image_file("FacesDatabase/Tymoffi.png")
 tymoffi_face_encoding = face_recognition.face_encodings(tymoffi_image)[0]
 
-owen_image = face_recognition.load_image_file("facesDatabase/Owen.png")
+owen_image = face_recognition.load_image_file("FacesDatabase/Owen.png")
 owen_face_encoding = face_recognition.face_encodings(owen_image)[0]
 
-juan_image = face_recognition.load_image_file("facesDatabase/Juan.png")
+juan_image = face_recognition.load_image_file("FacesDatabase/Juan.png")
 juan_face_encoding = face_recognition.face_encodings(juan_image)[0]
+
 
 known_face_encodings = [
     gabby_face_encoding,
@@ -80,71 +81,76 @@ face_names = []
 process_this_frame = True
 students = []
 
-
-t = time.localtime()
+#write video
+fps = int(stream.get(cv2.CAP_PROP_FPS))
+t= time.localtime()
 current_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
+output = cv2.VideoWriter('videoStorage/' + current_time + '.avi',cv2.VideoWriter_fourcc('M','J','P','G'),fps,(70,70))
 
-attendanceFile = open("attendance.txt","w")
-logsFile = open("logs.txt","w")
+attendanceFile = open("DataTxtFiles/attendance.txt","w")
+logsFile = open("DataTxtFiles/logs.txt","w")
 
 
-# while True: 
-    # if stream.isOpened():
-    #     break
-    # print("camera not found")
-    # time.sleep(10)
-
+while True: 
+    if stream.isOpened():
+        break
+    print("camera not found")
+    time.sleep(10)
 while True:
-    # connected = stream.isOpened()
-    # if not connected: 
-    #     print("connection lost: retrying in 10 seconds")
-    #     time.sleep(10)
-    #     continue 
+    connected = stream.isOpened()
+    if not connected: 
+        print("connection lost: retrying in 10 seconds")
+        time.sleep(10)
+        continue 
     ret, frame = stream.read()
     if process_this_frame:
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-                students.append(name)
-                utc_time = datetime.now(timezone('UTC'))
-                est_time = utc_time.astimezone(timezone('US/Eastern'))
-                time_string = est_time.strftime("%I:%M:%S %p")
-                logsFile.write(name + " , " + time_string + "\n")
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                    students.append(name)
+                    utc_time = datetime.now(timezone('UTC'))
+                    est_time = utc_time.astimezone(timezone('US/Eastern'))
+                    time_string = est_time.strftime("%I:%M:%S %p")
+                    logsFile.write(name + " , " + time_string + "\n")
                 face_names.append(name)
+    #!!!!!!!!!!!
+    #TO DO:CONDITIONAL FOR IF FACE IS FOUND LONGER THAN THRESHOLD THEN DOC IT
+    process_this_frame = not process_this_frame
 
-        process_this_frame = not process_this_frame
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (203, 192, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (203, 192, 2550), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 4, bottom - 4), font, 1.0, (255, 255, 255), 1)
 
-        # for (top, right, bottom, left), name in zip(face_locations, face_names):
-        #     top *= 4
-        #     right *= 4
-        #     bottom *= 4
-        #     left *= 4
-        #     cv2.rectangle(frame, (left, top), (right, bottom), (203, 192, 255), 2)
-        #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (203, 192, 255), cv2.FILLED)
-        #     font = cv2.FONT_HERSHEY_DUPLEX
-        #     cv2.putText(frame, name, (left + 4, bottom - 4), font, 1.0, (255, 255, 255), 1)
 
-        #show webcam 
-        cv2.imshow('Webcam', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    cv2.imshow('Webcam', frame)
+    output.write(frame)
 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
 
 inLab = list(set(students))
 for _ in inLab: 
     attendanceFile.write(_ + " was in the lab\n")
 
+
+
 attendanceFile.close()
 logsFile.close()
 stream.release()
+output.release()
 cv2.destroyAllWindows()
-
--- face_rec/parse.py
+++ face_rec/parse.py
@@ -1,23 +1,14 @@
 import faceRec
 from datetime import datetime
 import _strptime
-import schedule 
 from datetime import timedelta
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-
-# Load credentials 
-# credentials = service_account.Credentials.from_service_account_file('credentials.json')
-# sheets_api = build('sheets', 'v4', credentials=credentials)
-
 
 def updateFile(): 
-    #opens finalData.txt to append
-    finalFile = open("finalData.txt","a")
+    finalFile = open("DataTxtFiles/finalData.txt","a") #open as append? 
     today = datetime.now().date()
-    date = today.strftime('\n%d-%m-%Y')
+    date = today.strftime('\n%m/%d/%Y')
 
-    with open("logs.txt", "r") as f:
+    with open("DataTxtFiles/logs.txt", "r") as f:
         dict = {}
         lines = f.readlines()
         for line in lines:
@@ -28,17 +19,15 @@ def updateFile():
                 dict[name] = {"Arrived": time, "Left": time}
             else:
                 dict[name]["Left"] = time
-
-
     for key in dict:
         arrival_time = dict[key]["Arrived"]
         departure_time = dict[key]["Left"]
         arrival_time_obj = datetime.strptime(arrival_time, '%I:%M:%S %p')
         departure_time_obj = datetime.strptime(departure_time, '%I:%M:%S %p')
         total_time = departure_time_obj - arrival_time_obj
-        finalFile.write(f"{date} \n \n{key} \n Arrival:{arrival_time} \n Departure:{departure_time} \n Total:{total_time} \n")
-        # updateGoogleSheet('1Bl6i6xGl2_0BQjh1pWYwI8ko4__jJZfY3hEYDO-3cpE', 'finalData.txt')
+        finalFile.write(f"{date}  \n{key} \n Arrival:{arrival_time} \n Departure:{departure_time} \n Total:{total_time} \n")
 
+updateFile()
 # schedule.every(24).hours.do(updateFile)
 
 # #wait 3 mins before checking for a scheduled rule
@@ -46,33 +35,3 @@ def updateFile():
 #     schedule.run_pending()
 #     time.sleep(180) 
 
-
-
-# def updateGoogleSheet(sheet_id, data):
-#     # Read the contents of the "finalData.txt" file
-#     with open(data, "r") as file:
-#         lines = file.readlines()
-    
-#     spreadsheet_data = []
-#     for line in lines:
-#         row = [item.strip() for item in line.split('\n')]
-#         spreadsheet_data.extend(row)
-
-#     # Prepare the Google Sheets API request
-#     body = {
-#         'values': spreadsheet_data
-#     }
-    
-#     # Specify the sheet and range to update 
-#     range_name = 'Sheet1!A1'  
-#     result = sheets_api.spreadsheets().values().update(
-#         spreadsheetId=sheet_id,
-#         range=range_name,
-#         valueInputOption='RAW',
-#         body=body
-#     ).execute()
-    
-#     print(f'{result.get("updatedCells")} cells updated in Google Sheet.')
-
-            
-            
